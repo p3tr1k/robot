fe98b8bd8aab540623c4a8f03bceb35ab144d0ac
p3tr1k <petersabo.mail@gmail.com>
2026-07-08 16:59:16 +0200

    Update base rotation limits for symmetry

--- arm.py
+++ arm.py
@@ -54,8 +54,8 @@ ELBOW_SAFE_MIN = 30 # Upraviteľný limit, aby lakeť neschádzal príliš dole
 ELBOW_SAFE_MAX = 180
 
 # Špecifické limity pre Rotáciu základne (aby nenarážalo do kamery na chrbte)
-ROTATE_SAFE_MIN = 40 # Limit pre rotáciu vľavo/vzad
-ROTATE_SAFE_MAX = 140 # Limit pre rotáciu vpravo/vzad
+ROTATE_SAFE_MIN = 55 # Limit pre rotáciu vľavo/vzad (symetricky odvodené)
+ROTATE_SAFE_MAX = 145 # Extrémny limit vpravo podľa používateľa
 
 # Predvolené (stredové/oddychové) uhly
 default_angles = {
